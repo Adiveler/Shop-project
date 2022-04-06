a8e860e499656220b256fa55ab24ad6e991f5ef2
Adiveler <58662472+Adiveler@users.noreply.github.com>
2022-04-07 01:51:01 +0300

    Add files via upload

---
+++ Shop/models/VideoGame.js
@@ -0,0 +1,14 @@
+export default class VideoGame{
+
+    constructor(id, name, genre, publisher_company, developer_company, year, price, image, description){
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+        this.publisher_company = publisher_company;
+        this.developer_company = developer_company;
+        this.year = year;
+        this.price = price;
+        this.image = image;
+        this.description = description;
+    }
+}
